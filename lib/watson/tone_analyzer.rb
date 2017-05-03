2f3be68720570f07f1b86acb5ddcaefa1094c34a
Ankit Samarthya <ankitsamar@outlook.com>
2017-05-04 01:45:01 +0530

    space added

--- lib/watson/tone_analyzer.rb
+++ lib/watson/tone_analyzer.rb
@@ -3,14 +3,14 @@ require 'httparty'
 
 module Watson
   class ToneAnalyzer
-    def initialize(username, password, version='v3', version_date='2016-05-19')
+    def initialize(username, password, version = 'v3', version_date = '2016-05-19')
       @username = username
       @password = password
       @url = "https://gateway.watsonplatform.net/tone-analyzer/api/#{version}/tone?version=#{version_date}"
     end
 
-    def tone(options={})
-      return HTTParty.post(
+    def tone(options = {})
+      HTTParty.post(
         @url,
         headers: { 'Content-Type': 'application/json' },
         body: options.to_json,
